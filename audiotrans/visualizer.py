fba2a134e3f88123d1136405dee9a53e4daab64b
keik <k4t0.kei@gmail.com>
2016-08-28 19:11:51 +0900

    Abstract each draw methods

--- audiotrans/visualizer.py
+++ audiotrans/visualizer.py
@@ -35,66 +35,83 @@ class Visualizer():
 
         if chart_type == 'freq':
             self.xhop = 30
-            self.draw = self._draw_freq
+            self.draw_data = draw_freq
             self.axes.set_ylim(-1, 1)
             self.axes.set_xlim(0, 1000)
 
         elif chart_type == 'spec':
-            self.draw = self._draw_spec
+            self.xhop = 1
+            self.draw_data = draw_spec
             self.axes.set_ylim(0, 100)
             self.axes.set_xticks(np.linspace(0, 100, 6))
             self.axes.set_xticklabels(np.linspace(0, int(framerate / 2), 6))
 
         elif chart_type == 'specflux':
             self.xhop = 1
-            self.draw = self._draw_specflux
+            self.draw_data = draw_specflux
             self.axes.set_ylim(0, 100)
-            self.axes.set_xlim(0, 1000)
+            self.axes.set_xlim(0, 200)
 
-        self.fig.show()
+        self.background = None
 
-        self.background = self.fig.canvas.copy_from_bbox(self.axes.bbox)
+    def draw(self, data):
+        if self.background is None:
 
-        # TODO: modular
-        self.lines = self.axes.plot((np.zeros((1000, 2))))
+            # update draw method to omit condition evaluation for performance
+            if type(data) is tuple:
+                series_num = len(data)
+                self.draw = self._draw_multiseries
+            else:
+                series_num = 1
+                self.draw = self._draw_series
 
-    def _draw_freq(self, data):
+            # self.lines = self.axes.plot((np.zeros((self.axes.get_xlim()[1], series_num))))
+            self.lines = self.axes.plot((np.zeros((0, series_num))))
+            self.axes.legend(np.arange(series_num))
+            self.fig.show()
+            self.background = self.fig.canvas.copy_from_bbox(self.axes.bbox)
+            xlen = int(self.axes.get_xlim()[1])
+            for l in self.lines:
+                l.set_data(np.arange(xlen), np.zeros(xlen))
+
+        self.draw(data)
+
+    def _draw_series(self, data):
         self.fig.canvas.restore_region(self.background)
-
-        if len(np.shape(data)) != 1:
-            raise TypeError('To draw frequency, input data must formed 1-D array')
-
-        d = data[::self.xhop]
-        prev_x, prev_y = self.lines[0].get_data()
-
-        self.lines[0].set_data(prev_x, np.append(prev_y[len(d):], d))
-        self.axes.draw_artist(self.lines[0])
+        self.draw_data(data[::self.xhop], self.axes, self.lines[0])
         self.fig.canvas.blit(self.axes.bbox)
 
-    def _draw_spec(self, data):
+    def _draw_multiseries(self, data):
         self.fig.canvas.restore_region(self.background)
-
-        dim = len(np.shape(data))
-        if dim == 1:
-            d = np.abs(data)
-        elif dim == 2:
-            d = np.abs(data.T[0])
-        self.axes.set_xlim(0, len(d))
-
-        self.lines[0].set_data(np.arange(len(d)), d)
-        self.axes.draw_artist(self.lines[0])
+        series_num = len(data)
+        for i in range(series_num):
+            self.draw_data(data[i][::self.xhop], self.axes, self.lines[i])
         self.fig.canvas.blit(self.axes.bbox)
 
-    def _draw_specflux(self, data):
-        specflux, threshold = data
-        self.fig.canvas.restore_region(self.background)
 
-        s, t = specflux[::self.xhop], (threshold[::self.xhop] + 10)
-        prev_s_x, prev_s_y = self.lines[0].get_data()
-        prev_t_x, prev_t_y = self.lines[1].get_data()
+def draw_freq(data, axes, line):
+    if len(np.shape(data)) != 1:
+        raise TypeError('To draw frequency, input data must formed 1-D array')
 
-        self.lines[0].set_data(prev_s_x, np.append(prev_s_y[len(s):], s))
-        self.lines[1].set_data(prev_t_x, np.append(prev_t_y[len(t):], t))
-        self.axes.draw_artist(self.lines[0])
-        self.axes.draw_artist(self.lines[1])
-        self.fig.canvas.blit(self.axes.bbox)
+    prev_x, prev_y = line.get_data()
+    new_y = np.append(prev_y[len(data):], data)
+    line.set_data(prev_x, new_y)
+    axes.draw_artist(line)
+
+
+def draw_spec(data, axes, line):
+    dim = len(np.shape(data))
+    if dim == 1:
+        d = np.abs(data)
+    elif dim == 2:
+        d = np.abs(data.T[0])
+
+    axes.set_xlim(0, len(d))
+    line.set_data(np.arange(len(d)), d)
+    axes.draw_artist(line)
+
+
+def draw_specflux(data, axes, line):
+    prev_x, prev_y = line.get_data()
+    line.set_data(prev_x, np.append(prev_y[len(data):], data))
+    axes.draw_artist(line)
